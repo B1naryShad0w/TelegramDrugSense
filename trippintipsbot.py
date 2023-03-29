53f1980fef54c6d5b9e7b267de042f43845ecf46
B1naryShad0w <33293794+B1naryShad0w@users.noreply.github.com>
2023-03-29 02:00:16 -0400

    Update trippintipsbot.py

--- trippintipsbot.py
+++ trippintipsbot.py
@@ -2,6 +2,7 @@
 import logging
 import openai
 import os
+import telegram
 from telegram import Update, ForceReply
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
@@ -12,7 +13,7 @@ telegram_api_key = "<API KEY>"
 
 
 #system message to openai
-openai_system = "You are a telegram bot named DrugSense who's goal is to advise students on safe drug usage. Don't try to discourage taking drugs, rather tell them how to do so safely. Try to insert emojis when appropriate but don't repeat emojis much. Cursing and NSFW humor is encouraged. Try to stay under 150 tokens."
+openai_system = "You are a telegram bot named TrippinTips who's goal is to advise students on safe drug usage. Don't try to discourage taking drugs, rather tell them how to do so safely. Try to insert emojis when appropriate but don't repeat emojis much. Cursing and NSFW humor is encouraged. Try to stay under 150 tokens."
 
 
 #setup debugging
@@ -26,6 +27,7 @@ logging.basicConfig(
 
 #start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=telegram.constants.ChatAction.TYPING)
     request = [
         {"role": "system", "content": openai_system + "greet " + update.effective_user.first_name},
         {"role": "user", "content": "who are you and what can you help me with?"}
@@ -41,11 +43,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info(response['usage'])
     logging.info(response['choices'][0]['message'])
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=response['choices'][0]['message']['content'], reply_markup=ForceReply())
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response['choices'][0]['message']['content'])
 
 
 #response from openai
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=telegram.constants.ChatAction.TYPING)
     try:
         request = [
                 {"role": "system", "content": openai_system + "no greetings"},
@@ -73,7 +76,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(telegram_api_key).build()
-    
+
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
 
@@ -81,3 +84,4 @@ if __name__ == '__main__':
     application.add_handler(echo_handler)
 
     application.run_polling()
+
